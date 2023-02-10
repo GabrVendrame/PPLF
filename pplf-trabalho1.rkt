2e971d2e43a749169679b64fb3c84cb9ead70de2
GabrVendrame <gabrielvendrame7@gmail.com>
2023-02-10 20:04:40 -0300

    Exercício 2 finalizado.

--- pplf-trabalho1.rkt
+++ pplf-trabalho1.rkt
@@ -42,7 +42,8 @@ reuniao1 17:30 18:00, reuniao2 14:30 18:00, retorna #t, reuniao2 ainda está oco
 ;; IMPLEMENTAÇÃO
 
 (struct horario (hora-inicial minuto-inicial hora-final minuto-final) #:transparent)
-#| Horário representa a reserva de hora para a sala de reunião.
+#|
+Horário representa a reserva de hora para a sala de reunião.
 hora-inicial: número - hora inicial da reserva (exemplo 9:00-12:00, a hora inicial seria 9).
 minuto-inicial: número - minuto inicial da reserva (no exemplo acima minuto inicial seria 0).
 hora-final: número - hora final da reserva (no exemplo anterior hora final seria 12).
@@ -50,23 +51,23 @@ minuto-final número - minuto final da reserva (no exemplo citado minuto final s
 |#
 
 (define (conflito? reuniao1 reuniao2)
-                   (cond
-                     [(= (horario-hora-inicial reuniao1) (horario-hora-inicial reuniao2))
-                      (cond
-                        [(= (horario-minuto-inicial reuniao1) (horario-minuto-inicial reuniao2)) #t]
-                        [(< (horario-minuto-inicial reuniao1) (horario-minuto-inicial reuniao2))
-                         (cond
-                           [(<= (horario-minuto-final reuniao1) (horario-minuto-inicial reuniao2)) #f]
-                           [else #t])])]
-                     [(< (horario-hora-inicial reuniao1) (horario-hora-inicial reuniao2))
-                      (cond
-                        [(<= (horario-hora-final reuniao1) (horario-hora-inicial reuniao2)) #f]
-                        [else #t])]
-                     [else
-                      (cond
-                        [(>= (horario-hora-inicial reuniao1) (horario-hora-final reuniao2)) #f]
-                        [else #t])]))
-                      
+  (cond
+    [(= (horario-hora-inicial reuniao1) (horario-hora-inicial reuniao2))
+     (cond
+       [(= (horario-minuto-inicial reuniao1) (horario-minuto-inicial reuniao2)) #t]
+       [(< (horario-minuto-inicial reuniao1) (horario-minuto-inicial reuniao2))
+        (cond
+          [(<= (horario-minuto-final reuniao1) (horario-minuto-inicial reuniao2)) #f]
+          [else #t])])]
+    [(< (horario-hora-inicial reuniao1) (horario-hora-inicial reuniao2))
+     (cond
+       [(<= (horario-hora-final reuniao1) (horario-hora-inicial reuniao2)) #f]
+       [else #t])]
+    [else
+     (cond
+       [(>= (horario-hora-inicial reuniao1) (horario-hora-final reuniao2)) #f]
+       [else #t])]))
+
 ;; VERIFICAÇÃO 
 (examples
  (check-equal? (conflito? (horario 8 0 8 50) (horario 14 0 14 50)) #f)
@@ -88,10 +89,9 @@ Informações: localização atual do personagem e um comando.
 Representações:
 personagem é uma struct com o ponto (x, y), que representa a localização no tabuleiro, e a posição em que ele está virado.
 
-comando é uma string que pode assumir três valores:
-"direita".
-"esquerda".
-"avançar n".
+comando também é uma struct que pode assumir dois valores:
+"virar", que tera duas opções, "esquerda" e "direita"
+"avancar", que tem apenas a opção de avançar na posição em que está virado.
 |#
 
 #| ESPECIFICAÇÃO
@@ -102,18 +102,73 @@ Retorna a nova posição do personagem de acordo com o comando especificado.
          personagem
          comando) ...)
 Exemplos:
-personagem (2, 8) "norte", comando "direita", retorna personagem (2, 8) "leste".
-personagem (3, 5) "norte", comando "esquerda", retorna personagem (3, 5) "oeste".
-personagem (3, 2) "sul", comando "direita", retorna personagem (3, 2) "oeste".
-personagem (5, 4) "sul", comando "esquerda", retorna personagem (5, 4) "leste".
-personagem (1, 9) "leste", comando "direita", retorna personagem (5, 4) "sul".
-personagem (9, 10) "leste", comando "esquerda", retorna personagem (9, 10) "norte".
-personagem (1, 6) "oeste", comenado "direita", retorna personagem (1, 6) "norte".
-personagem (1, 2) "oeste", comenado "esquerda", retorna personagem (1, 2) "sul".
-personagem (4, 9) "norte", comando "avancar 1", retorna personagem (4, 10) "norte".
-personagem (7, 6) "sul", comando "avancar 2", retorna personagem (5, 6) "sul".
-personagem (6, 10) "leste", comando "avancar 2", retorna personagem (8, 10) "leste".
-personagem (9, 3) "oeste", comando "avancar 1", retorna personagem (8, 3) "oeste".
+personagem (2, 8) "norte", comando "virar" "direita", retorna personagem (2, 8) "leste".
+personagem (3, 5) "norte", comando "virar" "esquerda", retorna personagem (3, 5) "oeste".
+personagem (3, 2) "sul", comando "virar" "direita", retorna personagem (3, 2) "oeste".
+personagem (5, 4) "sul", comando "virar" "esquerda", retorna personagem (5, 4) "leste".
+personagem (1, 9) "leste", comando "virar" "direita", retorna personagem (5, 4) "sul".
+personagem (9, 10) "leste", comando "virar" "esquerda", retorna personagem (9, 10) "norte".
+personagem (1, 6) "oeste", comenado "virar" "direita", retorna personagem (1, 6) "norte".
+personagem (1, 2) "oeste", comenado "virar" "esquerda", retorna personagem (1, 2) "sul".
+personagem (4, 9) "norte", comando "avancar" 1, retorna personagem (4, 10) "norte".
+personagem (7, 6) "sul", comando "avancar" 2, retorna personagem (5, 6) "sul".
+personagem (6, 10) "leste", comando "avancar" 2, retorna personagem (8, 10) "leste".
+personagem (9, 3) "oeste", comando "avancar" 1, retorna personagem (8, 3) "oeste".
 |#
 
 ;; IMPLEMENTAÇÃO
+
+(struct personagem (x y direcao)#:transparent)
+#|
+personagem representa o jogador no tabuleiro.
+x: número - representa a coordenada x do jogador no tabuleiro.
+y: número - representa a coordenada y do jogador no tabuleiro.
+direcao: string - representa a direção cardeal em que o jogador está virado.
+|#
+
+(struct comando (acao valor)#:transparent)
+#|
+comando representa a próxima jogada do jogador no tabuleiro.
+acao: string - representa a ação do movimento escolhido, virar ou avançar.
+valor: caso acao = virar é uma string - representa a direção em que o jogador irá virar, esquerda ou direita.
+caso acao = avancar é um número - representa a quantidade de casas que o jogador irá avançar.
+|#
+
+(define (jogada jogador movimento)
+  (cond
+    [(equal? (comando-acao movimento) "virar")
+     (cond
+       [(equal? (comando-valor movimento) "direita")
+        (cond
+          [(equal? (personagem-direcao jogador) "norte") (struct-copy personagem jogador [direcao "leste"])]
+          [(equal? (personagem-direcao jogador) "sul") (struct-copy personagem jogador [direcao "oeste"])]
+          [(equal? (personagem-direcao jogador) "leste") (struct-copy personagem jogador [direcao "sul"])]
+          [else (struct-copy personagem jogador [direcao "norte"])])]
+       [else
+        (cond
+          [(equal? (personagem-direcao jogador) "norte") (struct-copy personagem jogador [direcao "oeste"])]
+          [(equal? (personagem-direcao jogador) "sul") (struct-copy personagem jogador [direcao "leste"])]
+          [(equal? (personagem-direcao jogador) "leste") (struct-copy personagem jogador [direcao "norte"])]
+          [else (struct-copy personagem jogador [direcao "sul"])])])]
+    [else
+     (cond
+       [(equal? (personagem-direcao jogador) "norte") (struct-copy personagem jogador [y (+ (personagem-y jogador) (comando-valor movimento))])]
+       [(equal? (personagem-direcao jogador) "sul") (struct-copy personagem jogador [y (- (personagem-y jogador) (comando-valor movimento))])]
+       [(equal? (personagem-direcao jogador) "leste") (struct-copy personagem jogador [x (+ (personagem-x jogador) (comando-valor movimento))])]
+       [else (struct-copy personagem jogador [x (- (personagem-x jogador) (comando-valor movimento))])])]))
+
+;; VERIFICAÇÃO
+
+(examples
+ (check-equal? (jogada (personagem 2 8 "norte") (comando "virar" "direita")) (personagem 2 8 "leste"))
+ (check-equal? (jogada (personagem 3 5 "norte") (comando "virar" "esquerda")) (personagem 3 5 "oeste"))
+ (check-equal? (jogada (personagem 3 2 "sul") (comando "virar" "direita")) (personagem 3 2 "oeste"))
+ (check-equal? (jogada (personagem 5 4 "sul") (comando "virar" "esquerda")) (personagem 5 4 "leste"))
+ (check-equal? (jogada (personagem 1 9 "leste") (comando "virar" "direita")) (personagem 1 9 "sul"))
+ (check-equal? (jogada (personagem 9 10 "leste") (comando "virar" "esquerda")) (personagem 9 10 "norte"))
+ (check-equal? (jogada (personagem 1 6 "oeste") (comando "virar" "direita")) (personagem 1 6 "norte"))
+ (check-equal? (jogada (personagem 1 2 "oeste") (comando "virar" "esquerda")) (personagem 1 2 "sul"))
+ (check-equal? (jogada (personagem 4 9 "norte") (comando "avancar" 1)) (personagem 4 10 "norte"))
+ (check-equal? (jogada (personagem 7 6 "sul") (comando "avancar" 2)) (personagem 7 4 "sul"))
+ (check-equal? (jogada (personagem 6 10 "leste") (comando "avancar" 2)) (personagem 8 10 "leste"))
+ (check-equal? (jogada (personagem 9 3 "oeste") (comando "avancar" 1)) (personagem 8 3 "oeste")))
